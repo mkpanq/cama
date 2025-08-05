cacef178dd5f17de07515f25bfb2c56e0f5fcc88
Marek Pankowski <17934750+mkpanq@users.noreply.github.com>
2025-08-05 17:31:14 +0200

    Agreement type

--- src/app/(dashboard)/institutions/_lib/agreement.ts
+++ src/app/(dashboard)/institutions/_lib/agreement.ts
@@ -1,3 +1,10 @@
-// Create agreement
-// Send request to API
-// And save it to the database - remember to save it together with user_id and institution_id
+// I've decided to not write reconfirmation (which is only for GB banks only) and allowed_scope - I always want the full scope
+type Agreement = {
+  id: string;
+  userId: string;
+  institutionId: string;
+  maxHistoricalDays: number;
+  validFor: number;
+  created: Date;
+  accepted: Date;
+};
