ef8574cf3b150d4292615ad10a6706a7364a8ff4
Marek Pankowski <17934750+mkpanq@users.noreply.github.com>
2025-08-15 20:04:49 +0200

    small removals in env example

--- src/db/client.ts
+++ src/db/client.ts
@@ -5,7 +5,7 @@ import postgres from "postgres";
 // TODO: Set default userId automatically, whenever user has active session?
 // TODO: Database connections limit - we need to handle the error that appeared during development: - ``[Error [PostgresError]: remaining connection slots are reserved for roles with the SUPERUSER attribute``
 export default async function getDBClient() {
-  const client = postgres(process.env.DATABASE_URL!);
+  const client = postgres(process.env.DATABASE_URL!, { prepare: false });
 
   return drizzle({ client });
 }
