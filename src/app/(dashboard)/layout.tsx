e446f39909eff8a6c2ae2e831a8e163e67e47b53
Marek Pankowski <17934750+mkpanq@users.noreply.github.com>
2025-08-15 21:24:52 +0200

    Remove imports

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -2,10 +2,6 @@ import {
   Disclosure,
   DisclosureButton,
   DisclosurePanel,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuItems,
 } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import type { Metadata } from "next";
